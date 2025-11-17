6b8ea0b4d8030f1aa8dd9788fdf798babedfe650
Loïc Délon <l.delon@proton.me>
2025-11-17 19:23:07 +0100

    Reviewed processing tasks order

--- jobs_manager/services/jobs_manager_service.py
+++ jobs_manager/services/jobs_manager_service.py
@@ -3,14 +3,16 @@ from typing import Dict, List, Any
 from jobs_manager.models import Job
 from jobs_manager.repositories.jobs_db_repository import JobsDBRepository
 from jobs_manager.repositories.local_files_repository import LocalFilesRepository
+from library.repositories.books_db_repository import BooksDBRepository
 from jobs_manager.tasks import run_inference_task
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
 class JobsManagerService:
-    def __init__(self, jobs_db_repo=JobsDBRepository, local_files_repo=LocalFilesRepository):
+    def __init__(self, jobs_db_repo=JobsDBRepository, local_files_repo=LocalFilesRepository, books_db_repo=BooksDBRepository):
         self.jobsDBRepository = jobs_db_repo()
         self.localFilesRepository = local_files_repo()
+        self.booksDBRepository = books_db_repo()
 
     def get_jobs(self) -> List[Job]:
         return self.jobsDBRepository.get_jobs()
@@ -28,7 +30,7 @@ class JobsManagerService:
         '''Only for testing purposes'''
         return run_inference_task()
 
-    def process_controller(self, job_data: Dict) -> None:
+    def prepare_inference(self, job_data: Dict) -> None:
         '''
         Orchestrates the whole processing of a job.
         '''
@@ -39,30 +41,44 @@ class JobsManagerService:
         if not os.path.exists(jobs_volumes_path):
             raise FileNotFoundError(f"Directory {jobs_volumes_path} does not exist")
 
-        # scanning available volumes of the title
+        # fetch the title books to work with, ordered
         if channel_layer:
             async_to_sync(channel_layer.group_send)(
                 'process_group',
                 {
                     'type': 'process.message',
-                    'message': 'Scanning books'
+                    'message': 'Getting books'
                 }
             )
-        jobs_volumes = self.localFilesRepository.scan(jobs_volumes_path)
-        print(jobs_volumes)
+        job_volumes_to_process = self.booksDBRepository.get_title_books_to_process(job_data['title_name'])
+        job_volumes_names = [volume.name for volume in job_volumes_to_process]
+        for volume in job_volumes_to_process:
+            print(str(volume.name))
 
-        # extracting books
+        # extracting books to process
         if channel_layer:
             async_to_sync(channel_layer.group_send)(
                 'process_group',
                 {
                     'type': 'process.message',
-                    'message': 'Extracting books'
+                    'message': 'Found to process: ' + str(job_volumes_names),
                 }
             )
         else:
             raise NotImplementedError("Channel layer is not available")
 
-        for job_volume in jobs_volumes:
-            self.localFilesRepository.extraction(job_data['title_name'], job_volume)
+        for volume in job_volumes_to_process:
+            self.localFilesRepository.extraction(jobs_volumes_path, str(volume.file_path))
+            #todo: implement inference task in async with built-in django background tasks backend
+            #run_inference_task()
+            if channel_layer:
+                async_to_sync(channel_layer.group_send)(
+                    'process_group',
+                    {
+                        'type': 'process.message',
+                        'message': 'Processing: ' + str(volume.name),
+                    }
+                )
+            else:
+                raise NotImplementedError("Channel layer is not available")
             break
